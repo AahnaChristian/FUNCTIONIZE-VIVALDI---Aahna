efa9aff0e2accebf4041d0719df4cd018b67e678
AahnaChristian <98975884+AahnaChristian@users.noreply.github.com>
2022-09-06 21:00:03 -0600

    Update main.js

--- main.js
+++ main.js
@@ -21,28 +21,50 @@ function seasonSelectChanged() {
 
   // Update page to match selected season
   if (season === "spring") {
-    seasonH2.innerHTML = "spring";
-    seasonH2.style.color = "#0E94D1";
-    seasonImg.src = "img/spring.jpg";
-    seasonAudio.src = "songs/vivaldi-spring.mp3";
-    document.body.style.backgroundColor = "#0E94D1";
+    setSpring();
   } else if (season === "summer") {
-    seasonH2.innerHTML = "summer";
-    seasonH2.style.color = "#1BA848";
-    seasonImg.src = "img/summer.jpg";
-    seasonAudio.src = "songs/vivaldi-summer.mp3";
-    document.body.style.backgroundColor = "#1BA848";
+    setSummer();
   } else if (season === "autumn") {
-    seasonH2.innerHTML = "autumn";
-    seasonH2.style.color = "#FE6732";
-    seasonImg.src = "img/autumn.jpg";
-    seasonAudio.src = "songs/vivaldi-autumn.mp3";
-    document.body.style.backgroundColor = "#FE6732";
+    setAutumn();
   } else if (season === "winter") {
-    seasonH2.innerHTML = "winter";
-    seasonH2.style.color = "#1C64B9";
-    seasonImg.src = "img/winter.jpg";
-    seasonAudio.src = "songs/vivaldi-winter.mp3";
-    document.body.style.backgroundColor = "#1C64B9";
+    setWinter();
   }
+}
+
+// Benefit - Modularity
+
+function setSeason(seasonName, seasonColor) {
+  seasonH2.innerHTML = seasonName;
+  seasonH2.style.color = seasonColor;
+  seasonImg.src = `img/${seasonName}.jpg`;
+  seasonAudio.src = `songs/vivaldi-${seasonName}.mp3`;
+  document.body.style.backgroundColor = seasonColor;
+}
+
+function setSpring() {
+  
+}
+
+function setSummer() {
+  seasonH2.innerHTML = "summer";
+  seasonH2.style.color = "#1BA848";
+  seasonImg.src = "img/summer.jpg";
+  seasonAudio.src = "songs/vivaldi-summer.mp3";
+  document.body.style.backgroundColor = "#1BA848";
+}
+
+function setAutumn() {
+  seasonH2.innerHTML = "autumn";
+  seasonH2.style.color = "#FE6732";
+  seasonImg.src = "img/autumn.jpg";
+  seasonAudio.src = "songs/vivaldi-autumn.mp3";
+  document.body.style.backgroundColor = "#FE6732";
+}
+
+function setWinter() {
+  seasonH2.innerHTML = "winter";
+  seasonH2.style.color = "#1C64B9";
+  seasonImg.src = "img/winter.jpg";
+  seasonAudio.src = "songs/vivaldi-winter.mp3";
+  document.body.style.backgroundColor = "#1C64B9";
 }
